2e64779b220ff8806524d9360c2d6ad5e266a1a2
Lucas Lima <57276011+LucasUfac@users.noreply.github.com>
2023-09-21 21:55:23 -0500

    a

---
+++ msclientes/src/main/java/io/github/gitlucaslima/msclientes/MsclientesApplication.java
@@ -0,0 +1,13 @@
+package io.github.gitlucaslima.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclientesApplication.class, args);
+	}
+
+}
