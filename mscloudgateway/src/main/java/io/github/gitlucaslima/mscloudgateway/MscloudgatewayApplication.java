95095df7580f4b2c5160c544f50887ba320d1b56
Lucas Lima <57276011+LucasUfac@users.noreply.github.com>
2023-10-12 00:42:45 -0500

    acrescentando gateway e o eureka server para roteamento

---
+++ mscloudgateway/src/main/java/io/github/gitlucaslima/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,25 @@
+package io.github.gitlucaslima.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+				.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+				.build();
+	}
+}
